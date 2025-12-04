3d0f3e171b6a1eb6a3e56ea1ca104580beec8595
sinuhajii02 <cleojoe10@gmail.com>
2025-12-04 09:31:58 +0100

    Setup YOLO + predict model

---
+++ model_inference.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+model = YOLO('yolo11m')
+
+train_result = model.predict('input_videos/B1606b0e6_1 (26).mp4', save=True)
+print(train_result[0])
+print("================================================")
+for box in train_result[0].boxes:
+    print(box)
