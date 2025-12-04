8ab5588bb3176bbbdb7d36cd54ebf8c256526e92
sinuhajii02 <cleojoe10@gmail.com>
2025-12-04 21:16:53 +0100

    added model roboflow

--- model_inference.py
+++ model_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolo11m')
+model = YOLO('models/best.pt')
 
 train_result = model.predict('input_videos/B1606b0e6_1 (26).mp4', save=True)
 print(train_result[0])
